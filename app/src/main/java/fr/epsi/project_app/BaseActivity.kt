8901eec9f7522a1bbc7e656108fbeb9e4e771ad8
oualid3 <73546426+oualid3@users.noreply.github.com>
2021-12-09 18:01:05 +0100

    splash+header

---
+++ app/src/main/java/fr/epsi/project_app/BaseActivity.kt
@@ -0,0 +1,23 @@
+package fr.epsi.project_app
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+
+    fun showBtnBack(){
+        val imageViewBack=findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility=View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+    }
+
+    fun setHeaderTitle(txt : String){
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.text = txt
+    }
+
+}
